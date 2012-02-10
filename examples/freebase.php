f95740738296eec4bf5f7031e2c4053b64ca119a
Nate Good <nate.good@showclix.com>
2012-02-09 20:57:24 -0500

    update examples

--- examples/freebase.php
+++ examples/freebase.php
@@ -2,12 +2,11 @@
     /**
      * Grab some The Dead Weather albums from Freebase
      */
-    namespace Httpful;
     require(__DIR__ . '/../lib/httpful.php');
 
     $uri = "https://www.googleapis.com/freebase/v1/mqlread?query=%7B%22type%22:%22/music/artist%22%2C%22name%22:%22The%20Dead%20Weather%22%2C%22album%22:%5B%5D%7D";
-    $response = Request::get($uri)
-        ->expectsType(Mime::JSON)
+    $response = \Httpful\Request::get($uri)
+        ->expectsJson()
         ->sendIt();
 
     echo 'The Dead Weather has ' . count($response->body->result->album) . " albums.\n";
--- examples/showclix.php
+++ examples/showclix.php
@@ -4,8 +4,8 @@ require(__DIR__ . '/../lib/httpful.php');
 
 // Get event details for a public event
 $uri = "http://api.showclix.com/Event/8175";
-$response = Httpful\Request::get($uri)
-    ->expectsType(Httpful\Mime::JSON)
+$response = \Httpful\Request::get($uri)
+    ->expectsType(\Httpful\Mime::JSON)
     ->sendIt();
 
 // Print out the event details
