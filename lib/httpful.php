0f93c1e6ad608bd65b04fff10be49d7cbbc6aced
Nate Good <nate.good@showclix.com>
2012-02-12 00:32:36 -0500

    Methods added to HTTP class to help categorize methods

--- lib/httpful.php
+++ lib/httpful.php
@@ -64,6 +64,55 @@ class Http {
     const PUT       = 'PUT';
     const DELETE    = 'DELETE';
     const OPTIONS   = 'OPTIONS';
+    
+    /**
+     * @return array of HTTP method strings
+     */
+    public static function safeMethods() {
+        return array(self::HEAD, self::GET, self::OPTIONS);
+    }
+    
+    /**
+     * @return bool
+     * @param string HTTP method
+     */
+    public static function isSafeMethod($method) { return in_array(self::safeMethods()); }
+    
+    /**
+     * @return bool
+     * @param string HTTP method
+     */
+    public static function isUnsafeMethod($method) { return !in_array(self::safeMethods()); }
+    
+    /** 
+     * @return array list of (always) idempotent HTTP methods
+     */
+    public static function idempotentMethods() {
+        // Though it is possible to be idempotent, POST
+        // is not guarunteed to be, and more often than
+        // not, it is not.
+        return array(self::HEAD, self::GET, self::PUT, self::DELETE, self::OPTIONS);
+    }
+    
+    /**
+     * @return bool
+     * @param string HTTP method
+     */
+    public static function isIdempotent($method) { return in_array(self::safeidempotentMethodsMethods()); }
+    
+    /**
+     * @return bool
+     * @param string HTTP method
+     */
+    public static function isNotIdempotent($method) { return !in_array(self::idempotentMethods()); }
+    
+    /**
+     * @return array of HTTP method strings
+     */
+    public static function canHaveBody() {
+        return array(self::POST, self::PUT, self::OPTIONS);
+    }
+    
 }
 
 class Response {
