cdb8e44d58123350181675216a10dba2f7a6de9d
Prut Udomwattawee <apzentral@gmail.com>
2013-12-19 16:29:18 -0800

    Null is also a valid JSON data.

--- src/Httpful/Handlers/JsonHandler.php
+++ src/Httpful/Handlers/JsonHandler.php
@@ -22,7 +22,7 @@ class JsonHandler extends MimeHandlerAdapter
     public function parse($body)
     {
         $body = $this->stripBom($body);
-        if (empty($body))
+        if (empty($body) || $body === 'null')
             return null;
         $parsed = json_decode($body, $this->decode_as_array);
         if (is_null($parsed) && 'null' !== strtolower($body))
