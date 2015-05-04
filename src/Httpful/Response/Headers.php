0e38e4fdd2c490c8278a37386a7f731717472c17
Jochen Schäfer <jochen.schaefer@medicomp.de>
2015-05-04 18:19:49 +0200

    Solves issue #170: HTTP Header parsing is inconsistent

--- src/Httpful/Response/Headers.php
+++ src/Httpful/Response/Headers.php
@@ -13,14 +13,24 @@ final class Headers implements \ArrayAccess, \Countable {
 
     public static function fromString($string)
     {
-        $lines = preg_split("/(\r|\n)+/", $string, -1, PREG_SPLIT_NO_EMPTY);
-        array_shift($lines); // HTTP HEADER
-        $headers = array();
-        foreach ($lines as $line) {
-            list($name, $value) = explode(':', $line, 2);
-            $headers[strtolower(trim($name))] = trim($value);
+        $headers = preg_split("/(\r|\n)+/", $string, -1, \PREG_SPLIT_NO_EMPTY);
+        $parse_headers = array();
+        for ($i = 1; $i < count($headers); $i++) {
+            list($key, $raw_value) = explode(':', $headers[$i], 2);
+            $key = trim($key);
+            $value = trim($raw_value);
+            if (array_key_exists($key, $parse_headers)) {
+                // See HTTP RFC Sec 4.2 Paragraph 5
+                // http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
+                // If a header appears more than once, it must also be able to
+                // be represented as a single header with a comma-separated
+                // list of values.  We transform accordingly.
+                $parse_headers[$key] .= ',' . $value;
+            } else {
+                $parse_headers[$key] = $value;
+            }
         }
-        return new self($headers);
+        return new self($parse_headers);
     }
 
     public function offsetExists($offset)
