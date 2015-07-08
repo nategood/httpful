d573ded1ca440aa493d58aab49551c066794a0c6
Izaac Johansson <izz0j89@gmail.com>
2015-07-08 10:12:03 +0200

    Added options for json_decode variables depth and options

--- src/Httpful/Handlers/JsonHandler.php
+++ src/Httpful/Handlers/JsonHandler.php
@@ -9,10 +9,14 @@ namespace Httpful\Handlers;
 class JsonHandler extends MimeHandlerAdapter
 {
     private $decode_as_array = false;
+    private $depth = 512;
+    private $parse_options = 0;
 
     public function init(array $args)
     {
         $this->decode_as_array = !!(array_key_exists('decode_as_array', $args) ? $args['decode_as_array'] : false);
+        $this->depth = (array_key_exists('depth', $args) ? $args['depth'] : 512);
+        $this->parse_options = (array_key_exists('parse_options', $args) ? $args['parse_options'] : 0);
     }
 
     /**
@@ -24,7 +28,7 @@ class JsonHandler extends MimeHandlerAdapter
         $body = $this->stripBom($body);
         if (empty($body))
             return null;
-        $parsed = json_decode($body, $this->decode_as_array);
+        $parsed = json_decode($body, $this->decode_as_array, $this->depth, $this->parse_options);
         if (is_null($parsed) && 'null' !== strtolower($body))
             throw new \Exception("Unable to parse response as JSON");
         return $parsed;
