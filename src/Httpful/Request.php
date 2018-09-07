115a7209d87fbd5f25bc6d700d54a863e31a3176
Frank Rochlitzer <f.rochlitzer@b3-it.de>
2018-09-07 17:43:34 +0200

    * Fix issue 206
    Information disclosure on file post.
    The file with the absolute path was always transferred.
    
    Signed-off-by: Frank Rochlitzer <f.rochlitzer@b3-it.de>

--- src/Httpful/Request.php
+++ src/Httpful/Request.php
@@ -387,7 +387,7 @@ class Request
         foreach ($files as $key => $file) {
             $mimeType = finfo_file($finfo, $file);
             if (function_exists('curl_file_create')) {
-                $this->payload[$key] = curl_file_create($file, $mimeType);
+                $this->payload[$key] = curl_file_create($file, $mimeType, basename($file));
             } else {
                 $this->payload[$key] = '@' . $file;
 	            if ($mimeType) {
