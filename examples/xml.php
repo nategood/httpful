c4b9efbe1670adb2c33d7327f54ee96028eed570
Lars Moelleken <voku@voku-online.de>
2019-04-29 00:10:28 +0000

    Apply fixes from StyleCI

--- examples/xml.php
+++ examples/xml.php
@@ -2,10 +2,7 @@
 
 declare(strict_types=1);
 
-use Httpful\Handlers\JsonHandler;
 use Httpful\Mime;
-use Httpful\Request;
-use Httpful\Setup;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -20,4 +17,3 @@ $responseComplex = \Httpful\Client::getRequest($uri)
 // ---
 
 $responseSimple = \Httpful\Client::get($uri);
-
